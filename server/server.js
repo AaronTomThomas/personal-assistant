2d9fdb4311003ea554c9be1d7d4ed87cdef92c39
Aaron Thomas <126454177+AaronTomThomas@users.noreply.github.com>
2023-09-12 15:53:09 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -5,22 +5,6 @@ import { OpenAI } from "openai";
 
 import { google } from "googleapis";
 
-// const { OAuth2 } = google.auth.OAuth2(
-//   process.env.CLIENT_ID,
-//   process.env.CLIENT_SECRET,
-//   process.env.REDIRECT_URL
-// )
-
-// const oAuth2Client = new OAuth2(
-//   "952721833893-7jabu4ao987ll9aqkftm7atj66ra670l.apps.googleusercontent.com",
-//   "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"
-// );
-
-// oAuth2Client.setCredentials({
-//   refresh_token:
-//     "1//04ZMmR1PnaDD6CgYIARAAGAQSNwF-L9IrN-iqfOGEpG4-uA1IZt3qDC6Pv8smFA23-VPBAgJBDSLvZufc_w4MhgqgQGioiBcEz9w",
-// });
-
 dotenv.config();
 
 const openai = new OpenAI({
